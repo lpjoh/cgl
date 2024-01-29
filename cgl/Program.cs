a58befe4d947d49e78b0df7bec70a61d785dc544
crossfrog <devcrossfrog@gmail.com>
2024-01-29 18:48:21 -0500

    Add project files.

---
+++ cgl/Program.cs
@@ -0,0 +1,106 @@
+﻿const int width = 75, height = 25;
+
+bool[,]
+    grid1 = new bool[width, height],
+    grid2 = new bool[width, height],
+
+    currentGrid = grid1,
+    nextGrid = grid2;
+
+void PrintGrid()
+{
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            if (currentGrid[x, y] != nextGrid[x, y])
+            {
+                Console.SetCursorPosition(x, y);
+                Console.Write(currentGrid[x, y] ? "#" : " ");
+            }
+        }
+    }
+}
+
+bool InRange(int x, int y)
+{
+    return x >= 0 && x < width && y >= 0 && y < height;
+}
+
+int NeighborWeight(int x, int y)
+{
+    return InRange(x, y) && currentGrid[x, y] ? 1 : 0;
+}
+
+int NeighborCount(int x, int y)
+{
+    return
+        NeighborWeight(x - 1, y) +
+        NeighborWeight(x + 1, y) +
+        NeighborWeight(x, y - 1) +
+        NeighborWeight(x, y + 1) +
+        NeighborWeight(x - 1, y - 1) +
+        NeighborWeight(x + 1, y - 1) +
+        NeighborWeight(x - 1, y + 1) +
+        NeighborWeight(x + 1, y + 1);
+}
+
+Random random = new();
+
+for (int y = 0; y < height; y++)
+{
+    for (int x = 0; x < width; x++)
+    {
+        if (random.Next() % 2 == 0)
+        {
+            currentGrid[x, y] = true;
+        }
+    }
+}
+
+Console.Clear();
+
+while (true)
+{
+    PrintGrid();
+
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            int neighborCount = NeighborCount(x, y);
+
+            if (currentGrid[x, y])
+            {
+                if (neighborCount < 2)
+                {
+                    nextGrid[x, y] = false;
+                }
+                else if (neighborCount <= 3)
+                {
+                    nextGrid[x, y] = true;
+                }
+                else
+                {
+                    nextGrid[x, y] = false;
+                }
+            }
+            else
+            {
+                if (neighborCount == 3)
+                {
+                    nextGrid[x, y] = true;
+                }
+                else
+                {
+                    nextGrid[x, y] = false;
+                }
+            }
+        }
+    }
+
+    bool[,] oldGrid = currentGrid;
+
+    currentGrid = nextGrid;
+    nextGrid = oldGrid;
+}
